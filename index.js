3ad02181f04d4799d0e7ffda0fb964c7917cd79d
kevin <kevinke386@gmail.com>
2024-03-30 22:28:43 +0300

    listening

---
+++ index.js
@@ -0,0 +1,19 @@
+//let button = document.getElementById('button')
+//button.addEventListener="Click Me!",() => {
+//button.innerHTML = "Click Me!"
+//alert.is
+//})
+//const button = document.getElementById("button")
+//unction clickAlert(){
+//alert = "Click Me!"
+//}
+//input.addingEventListener('click', clickAlert)
+function addingEventListener() {
+    const button = document.getElementById("button");
+
+    function handleClick(event) {
+        alert = "Click Me!"
+    }
+    button.addEventListener("Click Me!", handleClick)
+
+}
